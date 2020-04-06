d8bb7424dd3618d3f3eab6c97112303424e852ee
Jeff Lu <jeff80119@gmail.com>
2020-04-06 23:25:49 +0800

    second commit

--- driving.py
+++ driving.py
@@ -1,16 +1,18 @@
 country = input('請問你是哪國人: ')
 age = input('輸入你的年齡: ')
-if int(age):
+if str.isdigit(age):
 	age = int(age)
-if country == '台灣':
-	if age >= 18:
-		print('你可以考駕照')
+	if country == '台灣':
+		if age >= 18:
+			print('你可以考駕照')
+		else:
+			print('你不可以考駕照')
+	if country == '美國':
+		if age >= 16:
+			print('你可以考駕照')
+		else:
+			print('你不可以考駕照')
 	else:
-		print('你不可以考駕照')
-if country == '美國':
-	if age >= 16:
-		print('你可以考駕照')
-	else:
-		print('你不可以考駕照')
+		print('你的國家未知')
 else:
-	print('你的國家未知')
+	print('年齡輸入錯誤')
